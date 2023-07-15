0e2ca58ed58fda8f9f74be9e63aadcd78a89cb99
stu32 <20224301003048@gtxy.edu.cn>
2023-07-15 09:14:06 +0800

    07-15更新1 （v0.0.3）

--- C语言学习/learn v1.0.cpp
+++ C语言学习/learn v1.0.cpp
@@ -2,19 +2,23 @@
 第一个程序 
 
 ***************************/
-#include <iostream>//库，以#include声明 
-
-int main()//主函数
-{
-	int a;//变量定义
-	a = 100;//变量表述
-	printf("我有 %s 元",a);//格式化输出
-	printf("你干嘛！~ 哎呦~\n");//调用printf函数输出
-	return 0;
-}
 
 //转义字符：
 // \n 换行  \a响铃 \t制表符  \v制表符  \r回车  \b删除一个字符 单独一个“/”表示换行
 //
 
-#define URL "wwww.github.com"
+#define URL "wwww.github.com";//以 #defin 为前置标识来表示常量
+char be = 100;
+
+
+
+#include <iostream>//库，以#include声明
+
+int main()//主函数
+{
+	int a;//变量定义
+	a = 100;//变量表述
+	printf("我有 %d 元\n",be);//格式化输出
+	printf("你干嘛！~ 哎呦~\n");//调用printf函数输出
+	return 0;
+}
