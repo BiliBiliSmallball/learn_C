3979cccec872440318c57ec273fc5178f16c34cc
stu32 <20224301003048@txy.edu.cn>
2023-07-17 11:02:41 +0800

    1.重构learn系列的前沿与结构 2.写第二课（v0.0.6）

--- C语言学习/learn v1.0.c
+++ C语言学习/learn v1.0.c
@@ -1,18 +1,24 @@
 /***************************
 第一个程序 
+1.结构
+头文件
+主函数{
+	return 0
+}
 
+2.转义字符
+\n 换行  \a响铃 \t制表符  \v制表符  \r回车  \b删除一个字符 单独一个“/”表示换行
 ***************************/
+#include <iostream>//库，以#include声明
+#include <studio.h>
 
 //转义字符：
 //\n 换行  \a响铃 \t制表符  \v制表符  \r回车  \b删除一个字符 单独一个“/”表示换行
+void less1(){
+	printf("第一个\t，第二个\n,第三个\v,第四个\r，第五个,,\b\n")
+}
 
 
-#define URL "wwww.github.com";//以 #defin 为前置标识来表示常量
-int be = 100;
-
-
-
-#include <iostream>//库，以#include声明
 
 int main()//主函数
 {
@@ -20,6 +26,7 @@ int main()//主函数
 	a = 100;//变量表述
 	printf("我有 %f 元\n",be);//格式化输出
 	printf("你干嘛！~ 哎呦~\n");//调用printf函数输出
+	less1()//调用函数（跟py一样）
 	return 0;
 }
 /*
