d63f9bd5cc8d8aa9dd72c9afbdae659202f8d2ba
stu32 <20224301003048@gtxy.edu.cn>
2023-07-19 09:38:34 +0800

    版本大更新 1.README补全（部分） 2.将“小甲鱼代码”放到C语言学习这一文件夹下面。 3.-c learn v1.3 (v0.0.14)

--- C语言学习/learn v1.2.c
+++ C语言学习/learn v1.2.c
@@ -1,6 +1,7 @@
 /*
 第三节课:
 1.标识符：sizeof
+2.字符/字符串/ASCII码。
 */
 
 //sizeof(名称)   用来查看数据体大小（所占尺寸）
@@ -8,6 +9,7 @@
 
 #include<stdio.h>
 #include<math.h>
+
 void _Sizeof(){
     char a = 'A';//定义一个char类型的变量a
     printf("a的类型是%d",sizeof(a));
@@ -21,7 +23,9 @@ void cf(){
 }
 
 //字符
-//本质上就是利用ASCII码进行交代的东西
+//本质上就是利用ASCII码进行交代的东西,ASCII码是16进制
+//00空（\0）;01标题;02-03:文本;响铃07；回车0D；7F：删除；
+//30-39：0-9；41-5A：大写A_Z ；62-7A：小写a-z；
 void str(){
 	char a = 'C';
 	printf("%c = %d\n",a,a);
@@ -30,7 +34,9 @@ void str(){
 //看结果
 int main(){
     _Sizeof();
+    printf("\n"); 
     cf();
+    printf("\n");
     str();
     system("pause"); 
     return 0;
