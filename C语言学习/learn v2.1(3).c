3a4aa1d178197d9c049bf42c6bf27f1048b05e98
stu32 <20224301003048@gtxy.edu.cn>
2023-07-21 16:09:07 +0800

    1. 新增一个object文件夹，用于存放实际应用的代码。 2.完成v2.1（V0.0.21）

--- C语言学习/learn v2.1(3).c
+++ C语言学习/learn v2.1(3).c
@@ -1,28 +1,60 @@
 /*
-第六节课（算法课）:
+第六节课（杂技）:
 1.if(三种)
-2.while
-3.switch
+2.while(开始判断，结束判断)
+3.for
+4.switch
 */ 
-
-//while (表达式)
+#include<stdio.h>
+//for语句
+//如果你写的非常多的你就会发现就会发现：有的时候循环只是为了能够计时
+//所以就有一些快捷的写法：
+// for (类型 i = 0; i < 总次数; i++)
 // {
 //     代码
-// }
-
-#include<stdio.h>
-int main(){
-	int i;
-	printf("请输入一个数字:");
-	scanf("%d",&i);
-	
-	//循环体 
-    while ( i <= 100)
+//}
+void first(){
+    for (int i = 0; i < 50; i++)
     {
-       i++;
-       printf("现在的i为: %d\n",i);
+        printf("现在是%d",i);
     }
-    system("pause");
-	return 0;
+    //for的三个参数都可以省略。
 }
 
+//switch
+//这个比较特殊，应该放在if那份文件里面，但是这个奇葩他没有什么表达式之类的东西！
+// 就是一个量，然后当到达对应值时就结束
+
+// switch 语句中的 expression 是一个常量表达式，必须是一个整型或枚举类型。
+// 在一个 switch 中可以有任意数量的 case 语句。每个 case 后跟一个要比较的值和一个冒号。
+// case 的 constant-expression 必须与 switch 中的变量具有相同的数据类型，且必须是一个常量或字面量。
+
+//格式
+// switch (判断量)
+// {
+// case /* 常量表达式*/:
+//     代码
+//     break;
+//     （或者）
+//     continue;
+// default:
+//     break;
+// }
+void secend(){
+    int a = 1;
+    while (i <=10 )
+    {
+    i++;
+    switch (i){
+        case 5:
+            printf("现在是5")
+            break;
+        case 10:
+            printf("现在是10")
+            break;
+        default:
+            printf("程序运行结束")
+            break;
+        }
+    } 
+}
