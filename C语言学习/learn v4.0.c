b3dcc40672d28dcc8fd2dec34ef7cf9c25639ea3
stu32 <20224301003048@gtxy.edu.cn>
2023-07-29 11:16:28 +0800

    又写了个文件说明函数传递值(v0.0.32)

--- C语言学习/learn v4.0.c
+++ C语言学习/learn v4.0.c
@@ -5,12 +5,15 @@
 *********************/ 
 #include<stdio.h>
 // 函数声明的关键字：void
-// void hanshu(传入参数1，类型 参数2){
+// dataType functionName (传入参数1，类型 参数2){
 
     // 封装代码
     // (return 返回参数)
 // }
-//函数封装结束
+// 其中
+// dataType 是返回值类型，它可以是C语言中的任意数据类型，例如 int、float、char 等。
+// functionName 是函数名，它是标识符的一种，命名规则和标识符相同。函数名后面的括号( )不能少。
+// 如果有返回值，在函数体中使用 return 语句返回。return 出来的数据的类型要和 dataType 一样。
 
 void hannshu(void){//void是空值
 
@@ -18,9 +21,9 @@ void hannshu(void){//void是空值
 
 }
 
-
 //带参函数：请见./object/等差数列计算；
 
+//形参和实参
 
 //函数调用(一般就是直接在mian()里面了)
 int main(){
--- C语言学习/object/比大小.c
+++ C语言学习/object/比大小.c
@@ -5,7 +5,23 @@
 //此为4.0的补充
 
 //第一种定义法（方便不出错）coid Max(int,int)
-//
-void Max(int x,int y){
+//可以先不用写出参数名字
 
+int Max(int x,int y){
+    if (x > y)
+    {
+        return x;//return 结果了，下面的代码就不再执行了。
+    }
+    else{
+        return y;
+    }
 }
+
+int main(){
+    int x,y,z;
+    printf("请输入两个整数（以逗号分隔）");
+    scanf("%d,%d",&x,&y);       //格式占位符以逗号分隔，输入时就要以逗号分隔两个整数
+    z =Max(x,y);                //函数值的导出；
+    printf("他们中较大的是%d",z);
+    return 0;
+} 
