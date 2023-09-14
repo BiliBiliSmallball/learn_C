e9c0949049535e72063a9bff8b249ea2e4e7a578
Ender_Zhu <zzy1535878824@163.com>
2023-09-14 14:42:24 +0800

    （0.2.1）草稿代码编辑

--- 草稿/radion_calculate.c
+++ 草稿/radion_calculate.c
@@ -1,24 +1,11 @@
 #include <stdio.h>  
 
 void one(){
-	//输入一个字符，返回这一字符的ASCII码，并求出其ASCII码相邻的两位且按顺序输出
-	char a;
-	printf("请输入一个字\n");
-	scanf("%c",&a);
-	printf("这个字是：%d\n",a);
-	printf("前一位是%c,该位是%c 后一位是%c\n",a-1,a,a+1);
+	//空代码
 }
 
 void two(){
-	//计算输入的四个浮点数的平均值
-	float a,b,c,d;double avg,sum;
-	
-	scanf("%f %f %f %f",&a,&b,&c,&d);
-	
-	avg = (a + b + c + d)/4;
-	sum = a + b + c + d;
-
-	printf("该生总分为 %f,该生平均分为 %f \n",sum,avg);
+	//空代码
 }
 
 int main(){
