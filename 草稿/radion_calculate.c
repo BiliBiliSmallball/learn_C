1d52390211accf09c564f179e6d9e3c7739067af
Ender_Zhu <zzy1535878824@163.com>
2023-09-14 14:31:28 +0800

    （v0.1.5）草稿更新

--- 草稿/radion_calculate.c
+++ 草稿/radion_calculate.c
@@ -1,20 +1,30 @@
 #include <stdio.h>  
-  
-void one() {  
-    float x=3.5;
-	int z=8;  
-    char finish;  
-    finish = x + z %3/4;  //在C语言中，当你尝试将一个浮点数（float）和一个整数（int）进行运算时，浮点数会被转换为整数。这是因为C语言不支持在整数和浮点数之间的自动类型转换。
-    printf("%.2f\n", finish);  
+
+void one(){
+	//输入一个字符，返回这一字符的ASCII码，并求出其ASCII码相邻的两位且按顺序输出
+	char a;
+	printf("请输入一个字\n");
+	scanf("%c",&a);
+	printf("这个字是：%d\n",a);
+	printf("前一位是%c,该位是%c 后一位是%c\n",a-1,a,a+1);
 }
 
 void two(){
-	int a;
-	a=1,a+=1,a+1,a++;
-	printf("结果为:%d",a); 
+	//计算输入的四个浮点数的平均值
+	float a,b,c,d;double avg,sum;
+	
+	scanf("%f %f %f %f",&a,&b,&c,&d);
+	
+	avg = (a + b + c + d)/4;
+	sum = a + b + c + d;
+
+	printf("该生总分为 %f,该生平均分为 %f \n",sum,avg);
 }
 
 int main(){
+	one();
+	printf("————第二题————\n"); 
 	two();
+	system("pause");
 	return 0;
 }
