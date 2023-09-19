e3d1293566d3db716458b47987c140e16d01777a
Ender_Zhu <zzy1535878824@163.com>
2023-09-19 20:17:48 +0800

    （v0.1.8）1.更改了结构，并且新增了1.4——函数的嵌套调用 2.将3-100素数的程序写注释

--- C语言学习/object/输出3-100的素数.c
+++ C语言学习/object/输出3-100的素数.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 
 int is_prime(int number) {
-    if (number < 2) {
+    //所有的返回值都有个大的思想，0代表错误，1代表正确
+
+
+    if (number < 2) {//小于2的数字不是素数
         return 0;
     }
     
     for (int i = 2; i < number; i++) {
+    /*这里我们采用欧拉筛法。先取余，余数为0的就是素数，否则返回0（不是）*/
         if (number % i == 0) {
             return 0;
         }
     }
-    
     return 1;
 }
 
 void print_primes(int start, int end) {
+    //主程序
     for (int i = start; i <= end; i++) {
         if (is_prime(i)) {
             printf("%d是一个素数\n", i);
@@ -22,10 +26,7 @@ void print_primes(int start, int end) {
     }
 }
 
-void two() {
-    print_primes(3, 300);
-}
 
 int main(){
-    tow();
+    print_primes(3, 100);
 }
--- 草稿/radion_calculate.c
+++ 草稿/radion_calculate.c
@@ -1,40 +1,14 @@
-#include <stdio.h>  
+#include <stdio.h>
+#include <math.h>
 
-void one(){
-	//输入一个数，并判断该数是奇数还是偶数
-	int i;
-	printf("请输入一个数。\n");
-	scanf("%d",&i);
-	if(i%2==0){
-		printf("这是一个偶数。\n");
-	}
-	else{
-		printf("这是一个奇数。\n");
-	}
-}
-//
-//void two(){
-//	//输出三到100的素数
-//	for(int i=3;i<=300;i++){
-//		if (i%i == 0)
-//		{		
-//		printf("%d是一个素数",i);
-//		}
-//	}
-//}
-
-void three(){
-	int i =1,sum=0; 
-	while(i <= 100){
-		sum = i+sum ;
-	}
-	printf("I的值为: %d",i);
+int main() {
+    int m,n,sum;                        //sum是为了算最后的合计
+    printf("请输入两个数字: m,n\n");
+    scanf("%d %d",&m,&n);
+    for (m; m <= n; m++)
+    {
+        
+    }
+    
 }
 
-int main(){
-	three();
-	printf("\n————第二题————\n"); 
-//	two();
-	system("pause");
-	return 0;
-}
