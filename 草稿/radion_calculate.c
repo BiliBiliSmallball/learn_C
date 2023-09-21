8ea59ba3829788b039b66675000717e76388f614
Ender_Zhu <zzy1535878824@163.com>
2023-09-21 14:40:15 +0800

    重新修正

--- 草稿/radion_calculate.c
+++ 草稿/radion_calculate.c
@@ -5,10 +5,19 @@ int main() {
     int m,n,sum;                        //sum是为了算最后的合计
     printf("请输入两个数字: m,n\n");
     scanf("%d %d",&m,&n);
-    for (m; m <= n; m++)
-    {
-        
-    }
     
+    //加个前置的条件判断
+    if(	m<=0 && m>n){
+    	printf("m是不合法的数字！");
+	}
+	else{
+    	for (; m <= n; m++)
+    	{
+        	sum = pow(m,2) + 1/m;
+        	printf("%d \n",sum);
+    	}
+    printf("sum = %.6f\n",sum);
+	}
+    return 0;
 }
 
