97e571d058200327387f1863e6b14c846d4c138e
Ender_Zhu <zzy1535878824@163.com>
2023-09-21 15:56:08 +0800

    第四章的课后习题太经典了，所以单独写下

--- C语言学习/object/第四章课后题.c
+++ C语言学习/object/第四章课后题.c
@@ -29,31 +29,37 @@ void two(){
 	printf("\n----第二题----\n");
 }
 
-void three(){  
- //这题的思想是利用两个函数来解决这个问题  
- //第一个是用函数实现阶乘   
- int row(int i){    
-  int secend, sum = i;     
-  for(secend=1; secend < i; secend++){    
-   sum *= secend; //阶乘就是从1*2*3**。。。。n    
-  }    
-  return sum;  
- }    
-   
- int total;    
- total = row(5) + row(4) + row(3) + row(2) + row(1);    
- printf("最后的结果是：%d \n", total);    
- printf("----第三题----\n");    
+void three(){
+	//这题的思想是利用两个函数来解决这个问题
+	//第一个是用函数实现阶乘 
+	int row(int i){  
+ 		int secend, sum = 0;   
+ 		for(secend=1; secend <= i; secend++){  
+ 			sum *= secend; //阶乘就是从1*2*3**。。。。n  
+ 		}  
+ 	return sum;
+	}  
+ 	
+ 	int total;  
+ 	total = row(5) + row(4) + row(3) + row(2) + row(1);  
+ 	printf("最后的结果是：%d \n", total);  
+ 	printf("----第三题----\n");  
 }
 
 void four(){
-	
-}
-
+	int count = 0;
+	char ch;
+	scanf("%c",ch);
+	while((ch=getchar()) != '\n'){
+		count++;
+	}
+	printf("您一共写了 %d 个字符。",count);
+} 
 
 int main(){
 	one();
 	two();
 	three();
+	four();
 	return 0;
 }
