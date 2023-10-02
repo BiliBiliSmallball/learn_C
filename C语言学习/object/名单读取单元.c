bec5f2c0c9c879d30cd8e37c3c2083270aa030a8
Ender_Zhu <zzy1535878824@163.com>
2023-10-02 21:22:17 +0800

    （v0.1.20）名单读取总算正常了？

--- C语言学习/object/名单读取单元.c
+++ C语言学习/object/名单读取单元.c
@@ -1,47 +1,45 @@
 #include <stdio.h>  
 #include <stdlib.h>  
 #include <string.h>  
-  
-int main() {  
-    FILE *file;  
-    char filename[] = "./前置条件/destination_folder/students.txt";  
-    char *content;  
-    int length;  
-  
-    // 打开文件  
-    file = fopen(filename, "r");  
+
+typedef struct
+{ // 学生的信息可能包括姓名、学号等。可以使用结构体（struct）来存储这些信息。
+    char name[50];
+    int id;
+}Student;
+
+int main() 
+{	
+    FILE *file;
+    file = fopen("./前置条件/destination_folder/students.txt", "r");
     if (file == NULL) {  
-        printf("无法打开文件 %s\n", filename);  
+        printf("无法打开文件\n");  
         return 1;  
-    }  
+    }
+
+    
+    int num_students;  // 学生数量，需要根据实际情况确定  
+    Student *students;  
   
-    // 获取文件内容长度  
-    fseek(file, 0, SEEK_END);  
-    length = ftell(file);  
-    fseek(file, 0, SEEK_SET);  
-  
-    // 分配内存以存储文件内容  
-    content = (char *)malloc(length + 1);  
-    if (content == NULL) {  
+    // 动态分配内存以存储学生数据  
+    students = (Student *)malloc(num_students * sizeof(Student));//动态分配内存，以存储num_students个学生的信息。
+    if (students == NULL) {  
         printf("内存分配失败\n");  
         fclose(file);  
         return 1;  
-    }  
-  
-    // 读取文件内容到数组  
-    fread(content, 1, length, file);  
-    content[length] = '\0';  // 添加字符串结束符  
-  
-    // 关闭文件  
-    fclose(file);  
-  
-    // 现在，content 数组包含了TXT文件的内容  
-    // 你可以根据需要对数组进行操作或输出数组内容  
-  
-    printf("文件内容：\n%s\n", content);  
-  
-    // 释放内存  
-    free(content);  
-  
-    return 0;  
+    }
+	
+    while (fscanf(file, "%s %d", students[num_students].name, &students[num_students].id) != EOF) {//从文件中逐行读取学生的姓名和学号，并将其存储在结构体数组中。同时，递增num_students以记录学生的数量。
+    num_students++;  
+    }
+    fclose(file);
+
+    printf("读取到 %d 个学生信息：\n", num_students);
+    int i;
+    for (i=0; i < num_students; i++)
+    {
+        printf("学号：%d，姓名：%s\n", students[i].id, students[i].name);
+    }
+	system("pause"); 
+    return 0;
 }
